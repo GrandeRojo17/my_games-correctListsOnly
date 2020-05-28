b517fb5065c8836aaa2b9ef2299188bf49c57a07
granderojo17 <david.yennerell@gmail.com>
2020-05-28 00:22:39 -0500

    Vanilla version of List game completed.Next iteration coming soon

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/*Technically completed on 5/27/20
+Some additional notes here.
+*Create a README
+*Refactor the game.
+*add a start or home page.
+*instead of a list, maybe have different shapes.
+*add info inside a banner perhaps?
+*Add Categories or different list options
+*Save this version and create another but with a better UI.
+*Make the correctly chosen fonts more noticeable. 
+**Larger Fonts, blinking/fading/glowing fx
+**Shrink the UI to mitigate scrolling.
+** The  "Check-list" button should be visable from the start.
+
+
+
+*/
 const draggable_list = document.getElementById("draggable-list");
 const check = document.getElementById("check");
 const richestPeople = [
@@ -12,6 +29,7 @@ const richestPeople = [
   "Larry Page",
   "Larry Ellison",
 ];
+
 const listItems = [];
 let dragStartIndex;
 
@@ -25,7 +43,6 @@ function createList() {
     .sort((a, b) => a.sort - b.sort)
     .map((a) => a.value)
     .forEach((person, index) => {
-      console.log(person);
       const listItem = document.createElement("li");
       //Experiment with adding new classes for each action
       //   listItem.classList.add('over');
@@ -44,10 +61,10 @@ function createList() {
   addEventListeners();
 }
 
+//Drag and Drop functions
 function dragStart() {
   //this Key word pertains to the element.
   dragStartIndex = +this.closest("li").getAttribute("data-index");
-  console.log(dragStartIndex);
 }
 function dragEnter() {
   this.classList.add("over");
@@ -68,13 +85,34 @@ function dragDrop() {
 
   // console.log("Event: " ,dragStart);
 }
+
 function swapItems(fromIndex, toIndex) {
   const itemOne = listItems[fromIndex].querySelector(".draggable");
   const itemTwo = listItems[toIndex].querySelector(".draggable");
-  
+
   listItems[fromIndex].appendChild(itemTwo);
   listItems[toIndex].appendChild(itemOne);
+}
+//Timer to clear the red text.
+function clearAll() {
+  listItems.forEach((listItem, index) => {
+    listItem.classList.remove("wrong");
+  });
+  //The function 'clearAll' is designed to clean up the game from 'sensory overload'. ATM the correctly placed tiles are still mobile.
+}
+//check the order of list Items on 'click'
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
 
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+  setTimeout(clearAll, 5000);
 }
 
 function addEventListeners() {
@@ -92,3 +130,5 @@ function addEventListeners() {
     item.addEventListener("dragleave", dragLeave);
   });
 }
+
+check.addEventListener("click", checkOrder);
